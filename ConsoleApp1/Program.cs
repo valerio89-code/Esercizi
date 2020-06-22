9890f15005cadcd1778b8465241f9bd07621fd66
Valerio Valdes <valerio.valdes89@outlook.com>
2020-06-22 11:23:16 +0200

    secondo commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,13 +7,58 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    /*Inserire un numero da tastiera val. 
+    Il programma a questo punto deve chiedere all’utente di inserire un altro numero n e 
+    verificare se è maggiore di val o è minore di val.*/
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("prima stringa di prova");
-            Console.WriteLine("Prima riga da azure");
+            Point p = new Point();
+            p.X = 10;
+            p.Y = 11;
+            Console.WriteLine("Coordinate punto P : " + p);
+
+            Point p2 = new Point();
+            p2 = p;
+            Console.WriteLine("Coordinate punto P2 : " + p2);
+            p.X = 12;
+
+            Console.WriteLine("Coordinate punto P : " + p);
+            Console.WriteLine("Coordinate punto P2 : " + p2);
             Console.ReadLine();
+
+
+            TaskType newTask = TaskType.RiparazioneComputer;
+
+            switch (newTask)
+            {
+                case TaskType.InstallazioneSO:
+                    Console.WriteLine("Installazione SO");
+                    break;
+                case TaskType.RiparazioneComputer:
+                    Console.WriteLine("Riparazione PC");
+                    break;
+            }
+                                  
+            Console.WriteLine();
+        }
+
+        public enum TaskType
+        {
+            RiparazioneComputer,
+            InstallazioneSO,
+            SostituzioneDisco 
+        }
+
+        public struct Point
+        {
+            public float X;
+            public float Y;
+            public override string ToString()
+            {
+                return $"X = {X}, Y = {Y}";
+            }
         }
 
     }
