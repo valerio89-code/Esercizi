ca00dee3d822f7a868cbab82a1438ab524e791d9
Valerio Valdes <valerio.valdes89@outlook.com>
2020-06-23 13:52:23 +0200

    Fibonacci

--- Esercitazione/Program.cs
+++ Esercitazione/Program.cs
@@ -9,22 +9,30 @@ namespace Esercitazione
     class Program
     {
         /*
-Fare un programma che chieda all'utente N numeri diversi e poi ne calcoli la media,
-con N letto da tastiera;
+Fare un programma che chieda all'utente un numero N e stampi i primi
+N numeri della sequenza di fibonacci.
+0 1 1 2 3 5 8 13 21 34
  */
         static void Main(string[] args)
         {
-            Console.WriteLine("Inserire il numero N");
+            Console.WriteLine("Quanti numeri di fibonacci vuoi vedere?");
             int n = int.Parse(Console.ReadLine());
-            int somma = 0;
+            int f1 = 0;
+            int f2 = 1;
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("inserisci un numero");
-                somma += Convert.ToInt32(Console.ReadLine());
+                if (i > 1)
+                {
+                    int f = f2 + f1;
+                    f1 = f2;
+                    f2 = f;
+                    Console.WriteLine(f);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
-
-            double media = (double)somma / n;
-            Console.WriteLine($"La media dei {n} numeri inseriti è {media}");
         }
     }
 }
