fc0eafa5af3b5a78fbaf44ea6625becb217c3e7d
Valerio Valdes <valerio.valdes89@outlook.com>
2020-06-23 10:29:04 +0200

    Svolgimento esercizio 2

--- Esercitazione/Program.cs
+++ Esercitazione/Program.cs
@@ -13,12 +13,37 @@ namespace Esercitazione
     {
         static void Main(string[] args)
         {
-            int somma = 0;
-            for (int i = 0; i < 100; i++)
-            {
+            //leggo il primo numero
+            Console.WriteLine("Inserire un numero positivo");
+            string line = Console.ReadLine();
+            int a = Convert.ToInt32(line);
+            //leggo il secondo numero
+            Console.WriteLine("Inserire un altro numero positivo");
+            int b = int.Parse(Console.ReadLine());
 
+            int somma = 0;
+            //bool aMinoreUgualeB = a <= b;
+            //var estremoSuperiore = aMinoreUgualeB ? b : a;
+            //for (int i = aMinoreUgualeB ? a : b; i < estremoSuperiore; i++)
+            //{
+            //    somma = somma + i;
+            //}
+
+            if (a <= b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    somma = somma + i;
+                }
             }
-            Console.WriteLine(somma);
+            else
+            {
+                for (int i = b; i <= a; i++)
+                {
+                    somma = somma + i;
+                }
+            }
+            Console.WriteLine($"La somma dei numeri da {a} a {b} è {somma}");
         }
     }
 }
