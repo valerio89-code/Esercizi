275fed36975502b4ffdaf0680f7293bcac56ea3e
Valerio Valdes <valerio.valdes89@outlook.com>
2020-06-23 09:41:51 +0200

    Esempio ciclo for

--- Esercitazione/Program.cs
+++ Esercitazione/Program.cs
@@ -4,45 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- Scrivere un programma che stampi a video tutti i numeri pari compresi 
- tra due estremi a e b letti da tastiera. 
-Il programma deve dire anche quanti sono.
-*/
+ Calcolare la somma di tutti i numeri interi compresi 
+ fra due valori positivi dati in input a e b.
+ */
 namespace Esercitazione
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("inserisci un numero intero");
-            string readLine = Console.ReadLine();
-            int a = Convert.ToInt32(readLine);
-            Console.WriteLine("inserisci un altro numero intero");
-            readLine = Console.ReadLine();
-            int b = Convert.ToInt32(readLine);
-            if(a<b)
+            int somma = 0;
+            for (int i = 0; i < 100; i++)
             {
-                //se a è minore di b
-                int contatore = 0;
-                for (int i = a; i <= b; i++)
-                {
-                    if (i % 2 == 0)
-                    {
-                        Console.WriteLine(i + " è un numero pari");
-                        contatore++;
-                    }
-                }
 
-                Console.WriteLine($"il totale dei numeri pari compresi tra {a} e {b} è " + contatore);
             }
-            else //ALTRIMENTI (a maggiore di b e a = b)
-            {
-                Console.WriteLine("Errore: a maggiore di b");
-            }
-
-            
-
-            Console.ReadLine();
+            Console.WriteLine(somma);
         }
     }
 }
